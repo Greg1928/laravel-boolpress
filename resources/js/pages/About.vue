0efc68b7b2cd09c60d41ad405f3345db6557d4c3
Greg1928 <gregoriosortino@protonmail.com>
2022-07-28 16:40:06 +0200

    add about page

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Chi siamo</h1>
+    <p>About Us</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style>
+</style>
